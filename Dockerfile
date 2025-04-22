e2b14e6ebba8932ff30b2bd2b60cd47ee64cfde4
cscherr <Christoph.Scherr@newtec.de>
2025-04-22 17:17:49 +0200

    webshell

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM debian:bookworm
 RUN apt-get update && apt-get upgrade -y
-RUN apt-get install -y python3 python3-pip python3-flask procps
+RUN apt-get install -y python3 python3-pip python3-flask procps python3-flaskext.wtf
 RUN apt-get autoremove -y
 RUN yes | adduser dave
 RUN yes TESTPASS | passwd dave
 RUN mkdir /var/www
 COPY var/www /var/www
 COPY home/dave/* /home/dave
+COPY home/dave/.local /home/dave/.local
 RUN chown www-data:www-data /var/www -R
 USER dave
 EXPOSE 80 22
--- var/www/numfui/main.py
+++ var/www/numfui/main.py
@@ -1,15 +1,35 @@
 # TODO: make a little flaks service that is somehow offering os-command
 # injection
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, logging
+from logging import getLogger
+from wtforms import Form, IntegerField, StringField, validators
 import subprocess
 import os
 
 app = Flask("numfui")
 
+class FormatForm(Form):
+    number = StringField("number", [validators.Length(max=40)])
+    format = StringField("format", [validators.Length(min=3, max=3)])
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = FormatForm(request.form)
+    output: str = ""
+    if request.method == "POST":
+        logger = app.logger
+        if not form.validate():
+            logger.warning(f"The form was bad: {form.data}")
+        try:
+            cmd = f"/home/dave/.local/bin/numf {form.number.data}"
+            logger.info(f"cmd: {cmd}")
+            output: str = subprocess.check_output(cmd, shell=True).decode()
+            logger.info(f"output: {output}")
+        except subprocess.CalledProcessError as e:
+            logger.warning(f"The subprocess failed: {e}")
+            output = f"Error :( {e.stdout.decode()}"
+    return render_template("index.html", output=output)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
